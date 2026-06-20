37c85b1e1bc2e6e0dfeaf7d1ed105280d6c566c3
YudaAdiPratama <github.yuda@gmail.com>
2026-06-20 12:32:24 +0200

    cleos: change default wallet socket path from eosio-wallet to vex-wallet

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -155,7 +155,7 @@ std::string clean_output( std::string str ) {
 }
 
 string default_url = "http://127.0.0.1:8888";
-string default_wallet_url = "unix://" + (determine_home_directory() / "eosio-wallet" / (string(key_store_executable_name) + ".sock")).string();
+string default_wallet_url = "unix://" + (determine_home_directory() / "vex-wallet" / (string(key_store_executable_name) + ".sock")).string();
 string wallet_url; //to be set to default_wallet_url in main
 std::map<name, std::string>  abi_files_override;
 
